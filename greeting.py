092596b5633335ec9fbf002185b61f2d2498865b
shwetaa015 <shwetakawale15@gmail.com>
2025-05-17 16:12:36 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,4 @@
+name=input("Enter your first name: ")
+last=input("Enter your last name: ")
+
+print("Hello, "+ name + last +"!" +" Welcome to Python program.")
---
+++ operation.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+
+add=a+b
+print("Addition:",add)
+
+sub=a-b
+print("Substraction:",sub)
+
+mult=a*b
+print("Multiplication:",mult)
+
+div=a/b
+print("Division:",div)
